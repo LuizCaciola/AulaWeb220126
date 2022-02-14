9e3b5c2af29c5dbeb13095bb16c40250b4503f46
luizcaciola <luiz.caciola@gmail.com>
2022-02-13 21:47:12 -0300

    Termino do exercicio

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const usuarios = [];
 
-const usuario = {
-    nome: "Luiz",
-    sobrenome: "Caciola",
-}
-usuarios.push(usuario);
-
 app.get('/usuarios',(request, response) => {
     response.json({ usuarios })
+})
+
+app.post('/usuarios',(request, response) => {
+    //console.log(request.body);
+ 
+        const usuario = {
+        nome: request.body.nome,
+        sobrenome: request.body.sobrenome,
+    }    
+    usuarios.push(usuario);
+
+    response.json ({ usuario })
+})
+
+app.put('/usuarios/:id', (request, response) => {
+    const { id } = request.params;
+
+    const {nome, sobrenome} = request.body;
+
+    const usuario = {
+        nome,
+        sobrenome
+    }
+    usuarios[id] = usuario;
+
+    return response.json(usuario);
+})
+
+app.delete('/usuarios/:id', (request, response) => {
+    const { id } = req.params;
+
+    usuarios.splice(id, 1);
+
+    return res.status(204).send();
 })
 
 app.get('/produtos',(request, response) => {
